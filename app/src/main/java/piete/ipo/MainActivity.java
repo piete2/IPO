4b97541d7f731678e0db650cc108a64c979eb7ad
Antonio García Gómez <anto_riopar@hotmail.com>
2017-11-16 16:55:43 +0100

    New buttons

--- app/src/main/java/piete/ipo/MainActivity.java
+++ app/src/main/java/piete/ipo/MainActivity.java
@@ -5,40 +5,63 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
 
 public class MainActivity extends AppCompatActivity {
+    Button bA, ba;
+    int ver = 0;
+    TextView hora;
 
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+    Date date = new Date();
+
+    String fecha = dateFormat.format(date);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        hora = (TextView) findViewById(R.id.hora);
+        hora.setText(fecha);
+
     }
 
 
     public void camara(View v) {
-       // Intent camara_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Intent camara_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(cameraIntent, 0);
     }
 
-    public void galeria(View v){
+    public void galeria(View v) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(
                 "content://media/internal/images/media"));
         startActivity(intent);
     }
-public void k(View v){
-    Intent guardar = new Intent(getApplicationContext(), Emergencia.class);
-    startActivity(guardar);
-}
 
-    public void j(View v){
+    public void k(View v) {
+        Intent guardar = new Intent(getApplicationContext(), Emergencia.class);
+        startActivity(guardar);
+    }
+
+    public void j(View v) {
         Intent guardar = new Intent(getApplicationContext(), AccesosDirectos.class);
         startActivity(guardar);
     }
 
 }
+
 
 
 
