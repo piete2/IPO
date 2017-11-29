92a5cae565837193b793805bf374db87081399e6
Antonio García Gómez <anto_riopar@hotmail.com>
2017-11-29 08:38:59 +0100

    Contactos

---
+++ app/src/main/java/piete/ipo/apps.java
@@ -0,0 +1,13 @@
+package piete.ipo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class apps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps);
+    }
+}
